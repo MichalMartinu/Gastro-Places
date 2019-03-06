2dc39c08f7d3fbe24342c5836b0e2cd6d2adc41d
MichalMartinu <misanovy@gmail.com>
2019-03-06 13:35:09 +0100

    Initial Commit

---
+++ Gastro Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gastro Places
+//
+//  Created by Michal Martinů on 06/03/2019.
+//  Copyright © 2019 Michal Martinů. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
